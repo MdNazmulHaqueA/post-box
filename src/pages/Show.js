1b74b212bcc4b329985e9d5f84f621f382943f6b
MdNazmulHaqueA <naz089fvrict@gmail.com>
2021-09-14 06:56:58 +0600

    show page styling

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,23 +1,23 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/config';
+import { InfoBlock, ShowPageWrapper } from './Show.styled';
 
 const reducer = (prevState, action) => {
     switch (action.type) {
         case 'FETCH_SUCCESS': {
-            return {
-                isLoading: false,
-                error: null,
-                show: action.show,
-            };
+            return { isLoading: false, error: null, show: action.show };
         }
+
         case 'FETCH_FAILED': {
-            return {
-                ...prevState,
-                isLoading: false,
-                error: action.error,
-            };
+            return { ...prevState, isLoading: false, error: action.error };
         }
+
         default:
             return prevState;
     }
@@ -25,7 +25,7 @@ const reducer = (prevState, action) => {
 
 const initialState = {
     show: null,
-    isLoading: null,
+    isLoading: true,
     error: null,
 };
 
@@ -44,14 +44,11 @@ const Show = () => {
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
             .then((results) => {
                 if (isMounted) {
-                    //
                     dispatch({ type: 'FETCH_SUCCESS', show: results });
                 }
             })
             .catch((err) => {
                 if (isMounted) {
-                    // setError(err.message);
-                    // setIsLoading(false);
                     dispatch({ type: 'FETCH_FAILED', error: err.message });
                 }
             });
@@ -60,14 +57,42 @@ const Show = () => {
             isMounted = false;
         };
     }, [id]);
+
     console.log('show', show);
+
     if (isLoading) {
         return <div>Data is being loaded...</div>;
     }
     if (error) {
         return <div>Error occurred at: {error}</div>;
     }
-    return <div>this is show page</div>;
+    return (
+        <ShowPageWrapper>
+            <ShowMainData
+                image={show.image}
+                name={show.name}
+                rating={show.rating}
+                summary={show.summary}
+                tags={show.genres}
+            />
+            <InfoBlock>
+                <h2>Details:</h2>
+                <Details
+                    status={show.status}
+                    network={show.network}
+                    premiered={show.premiered}
+                />
+            </InfoBlock>
+            <InfoBlock>
+                <h2>Seasons:</h2>
+                <Seasons seasons={show._embedded.seasons} />
+            </InfoBlock>
+            <InfoBlock>
+                <h2>Cast:</h2>
+                <Cast cast={show._embedded.cast} />
+            </InfoBlock>
+        </ShowPageWrapper>
+    );
 };
 
 export default Show;
