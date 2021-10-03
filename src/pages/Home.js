edf8e356d614055c40e44af0a3a40c31f9e84cb1
MdNazmulHaqueA <naz089fvrict@gmail.com>
2021-10-03 20:07:59 +0600

    component optimization wiht memo and useCallback hook

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import ActorGrid from '../components/actor/ActorGrid';
 import CustomRadio from '../components/CustomRadio';
 import MainPageLayout from '../components/MainPageLayout';
@@ -11,15 +11,33 @@ import {
     SearchInput,
 } from './Home.styled';
 
+const renderResults = (results) => {
+    if (results && results.length === 0) {
+        return <div>No Results</div>;
+    }
+    if (results && results.length > 0) {
+        return results[0].show ? (
+            <ShowGrid data={results} />
+        ) : (
+            <ActorGrid data={results} />
+        );
+    }
+    return null;
+};
+
 const Home = () => {
     // const [input, setInput] = useState('');
     const [input, setInput] = useLastQuery();
     const [results, setResults] = useState(null);
     const [searchOption, setSearchOption] = useState('shows');
     const isShowsSearch = searchOption === 'shows';
-    const onInputChange = (ev) => {
-        setInput(ev.target.value);
-    };
+
+    const onInputChange = useCallback(
+        (ev) => {
+            setInput(ev.target.value);
+        },
+        [setInput]
+    );
 
     const onSearch = () => {
         apiGet(`/search/${searchOption}?q=${input}`).then((result) =>
@@ -33,23 +51,9 @@ const Home = () => {
         }
     };
 
-    const renderResults = () => {
-        if (results && results.length === 0) {
-            return <div>No Results</div>;
-        }
-        if (results && results.length > 0) {
-            return results[0].show ? (
-                <ShowGrid data={results} />
-            ) : (
-                <ActorGrid data={results} />
-            );
-        }
-        return null;
-    };
-
-    const onRadioChange = (ev) => {
+    const onRadioChange = useCallback((ev) => {
         setSearchOption(ev.target.value);
-    };
+    }, []);
 
     return (
         <MainPageLayout>
@@ -87,7 +91,7 @@ const Home = () => {
                     Search
                 </button>
             </SearchButtonWrapper>
-            {renderResults()}
+            {renderResults(results)}
         </MainPageLayout>
     );
 };
